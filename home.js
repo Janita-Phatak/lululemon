34d795520db7f80d84e981a8f87ef5852b80237a
Janita <phatakjanita@gmailcom>
2024-05-29 01:41:26 +0530

    fixed major issues

--- home.js
+++ home.js
@@ -79,6 +79,36 @@ document.addEventListener('DOMContentLoaded', () => {
   slideImage2('.product-container2', 0);
 });
 
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 4,
+  spaceBetween: 25,
+  loop: true,
+  // centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  //   dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // breakpoints:{
+  //     0: {
+  //         slidesPerView: 1,
+  //     },
+  //     520: {
+  //         slidesPerView: 2,
+  //     },
+  //     950: {
+  //         slidesPerView: 3,
+  //     },
+  // },
+});
+
 document.addEventListener('DOMContentLoaded', function() {
   // Hide the detailNav by default
   const detailNav = document.querySelector('.detailNav');
@@ -136,7 +166,3 @@ document.addEventListener('DOMContentLoaded', function() {
       }
   });
 });
-
-
-
-y
